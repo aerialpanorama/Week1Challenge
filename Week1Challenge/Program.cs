0a99fd8d85b737803f81be07458c1eb228de77f9
Rafael Rivera <rriveratechie@gmail.com>
2020-11-17 19:06:02 -0500

    initial commit

---
+++ Week1Challenge/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+       [TestMethod]
+        public void NameAndAge()
+        {
+            string firstname = "Rafael";
+            string lastname = "Rivera";
+            int age = 45;
+            Console.WriteLine(age + 7);
+            Console.WriteLine(age - 7);
+            Console.WriteLine(age * 7);
+            Console.WriteLine(age / 7);
+            Console.WriteLine(age % 7);
+
+        }
+        [TestMethod]
+        public void ListOfDates()
+        {
+            string[] movie = { "batman", "Die Hard", "little women" };
+
+        }
+        { 
+            List<DateTime> listOfDates = new List<DateTime>();
+        Datetime now = DateTime.Now;
+        listOfDates.Add(today);
+            listOfDates.add(new DateTime(2020,12,25));
+            listOfDates.add(new DateTime(2020,3,12));
+            }
+
+        [TestMethod]
+        public void HowMuchSleepDidYouGet()
+         { Console.WriteLine("How Much Sleep Did You get?????");
+        int hours = 4;
+        if (hours >= 10)
+        {
+            Console.WriteLine("Wow, that is a lot of sleep"); ;
+        }
+
+        else if (hours >= 8 && hours < 10)
+        {
+            Console.WriteLine("You Should Be Well Rested");
+
+        }
+        else if (hours > 4 && hours < 8)
+        {
+            Console.WriteLine("Bummer");
+
+        }
+        else
+        {
+            Console.WriteLine("Oh man get some sleep");
+        }
+        [TestMethod]
+        public void HowHasYourDayBeen()
+        {
+            string userday = "Okay";
+            switch (userday)
+            {
+                case "Great":
+                    Console.WriteLine("That is Great");
+                    break;
+                case "Good":
+                    Console.WriteLine("it is a day to be good on");
+                    break;
+                case "Okay":
+                    Console.WriteLine("Hope it gets better");
+                case ":(":
+                    Console.WriteLine("I guess this is us now");
+                    break;
+                default:
+                    Console.WriteLine("Please enter something above");
+
+            }
+        }
+    }[TestMethos]
+    public void Supercalafragilistexpialidotioous()
+    { string name = "Supercalafragilistexpialidotioous";
+        foreach (char letter in name)
+        {
+            if (letter == 'i')
+                Console.WriteLine((letter));
+        }
+        else
+        { Console.WriteLine("not an i" ); 
+        }
+            ;
+        }
+
+}
+   
---
+++ Week1Challenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Week1Challenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week1Challenge")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c9d767d8-195e-4aad-b8d2-00e01e86cb66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
